ed41f8366460510eb02919aff34a7718b2aefb12
Angoose <68403129+Angoooose@users.noreply.github.com>
2022-02-01 17:09:54 -0600

    Initializing Firestore

--- src/index.tsx
+++ src/index.tsx
@@ -4,6 +4,13 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import { initializeApp } from 'firebase/app';
+import { getFirestore } from 'firebase/firestore';
+import firebaseConfig from './firebase.config';
+
+initializeApp(firebaseConfig);
+export const database = getFirestore();
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
