fe29f018056705793d5bc2a9a6dea3e52ee023a3
Angoose <68403129+Angoooose@users.noreply.github.com>
2022-02-02 16:49:13 -0600

    Updating Input styles

--- src/common/Input.tsx
+++ src/common/Input.tsx
@@ -8,6 +8,6 @@ export default forwardRef<HTMLInputElement, InputHTMLAttributes<HTMLInputElement
     return <input
         {...props}
         ref={ref}
-        className="bg-neutral-200 border-b-2 border-b-teal-400 rounded-sm outline-none my-2 py-1 px-2"
+        className="border shadow-sm border-teal-400 focus:ring-1 focus:ring-blue-400 focus:invalid:ring-red-500 focus:border-blue-400 invalid:border-red-500 focus:invalid:border-red-500 transition-all rounded-md outline-none my-2 py-1 px-2"
     />
 });
