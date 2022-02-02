d41454269e6f14432b2f1d3081d10538c2163888
Angoose <68403129+Angoooose@users.noreply.github.com>
2022-02-02 16:49:20 -0600

    Updating Button styles

--- src/common/Button.tsx
+++ src/common/Button.tsx
@@ -3,6 +3,6 @@ import { ButtonHTMLAttributes } from 'react';
 export default function Button(props: ButtonHTMLAttributes<HTMLButtonElement>) {
     return <button 
         {...props}
-        className="bg-teal-400 text-white py-1 rounded-sm transition-opacity my-1 hover:opacity-80 disabled:opacity-50 disabled:cursor-not-allowed">{props.children}
+        className="bg-teal-600 text-white py-1 rounded-md transition-all my-1 hover:bg-teal-500 disabled:opacity-50 disabled:cursor-not-allowed">{props.children}
     </button>
 }
