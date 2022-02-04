67605ae976d72114b3c1996d5a2230a89ef8c18b
Angoose <68403129+Angoooose@users.noreply.github.com>
2022-02-04 16:31:58 -0600

    Updating Header colors

--- src/Header/Header.tsx
+++ src/Header/Header.tsx
@@ -3,11 +3,11 @@ import { ViewGridIcon, CogIcon } from '@heroicons/react/outline';
 export default function Header() {
     return (
         <header className="bg-neutral-100 flex text-2xl font-medium rounded-md shadow-md select-none">
-            <div className="flex py-2 w-3/6 justify-center items-center cursor-pointer transition-all rounded-l-md hover:bg-blue-400 hover:text-white border-blue-400 border-b-2">
+            <div className="flex py-2 w-3/6 justify-center items-center cursor-pointer transition-all rounded-l-md hover:bg-violet-500 hover:text-white border-violet-500 border-b-2 text-violet-500">
                 <ViewGridIcon className="w-7 mr-1"/>
                 Contacts
             </div>
-            <div className="flex py-2 w-3/6 justify-center items-center cursor-pointer transition-all rounded-r-md hover:bg-blue-400 hover:text-white">
+            <div className="flex py-2 w-3/6 justify-center items-center cursor-pointer transition-all rounded-r-md hover:bg-violet-500 hover:text-white">
                 <CogIcon className="w-7 mr-1"/>
                 Settings
             </div>
