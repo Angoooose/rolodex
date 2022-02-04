ca19a04cbb38f9346f83a610a7a1974c5cfa81e0
Angoose <68403129+Angoooose@users.noreply.github.com>
2022-02-04 16:29:05 -0600

    Adding label prop to Input component

--- src/common/Input.tsx
+++ src/common/Input.tsx
@@ -1,13 +1,17 @@
 import { forwardRef, InputHTMLAttributes, RefObject } from 'react';
 
-interface InputProps {
-    ref: RefObject<HTMLInputElement>,
+interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
+    label?: string,
+    isRequired?: boolean,
 }
 
-export default forwardRef<HTMLInputElement, InputHTMLAttributes<HTMLInputElement>>((props, ref) => {
-    return <input
-        {...props}
-        ref={ref}
-        className="border shadow-sm border-teal-400 focus:ring-1 focus:ring-blue-400 focus:invalid:ring-red-500 focus:border-blue-400 invalid:border-red-500 focus:invalid:border-red-500 transition-all rounded-md outline-none my-2 py-1 px-2"
-    />
+export default forwardRef<HTMLInputElement, InputProps>((props, ref) => {
+    return <div className="my-0.5">
+        {props.label && <div>{props.isRequired && <span className="text-red-600 font-bold">* </span>}{props.label}</div>}
+        <input
+            {...props}
+            ref={ref}
+            className={`${props.className} border shadow-sm border-teal-400 focus:ring-1 focus:ring-violet-500 focus:invalid:ring-red-500 focus:border-violet-500 invalid:border-red-500 focus:invalid:border-red-500 transition-all rounded-md outline-none py-1 px-2`}
+        />
+    </div>
 });
