90dad931df8e79d1769f1d48e8e8cb62829da829
Angoose <68403129+Angoooose@users.noreply.github.com>
2022-02-17 17:30:15 -0600

    Fixing Header style issues
    Bottom border had incorrect color.

--- src/Header/Header.tsx
+++ src/Header/Header.tsx
@@ -7,11 +7,11 @@ export default function Header() {
 
     return (
         <header className="bg-neutral-100 flex text-2xl font-medium rounded-md shadow-md select-none">
-            <Link to="/" className={`flex py-2 w-3/6 justify-center items-center cursor-pointer transition-all rounded-l-md hover:bg-violet-500 hover:border-violet-500 hover:text-white border-b-2 ${route.pathname === '/' ? 'border-violet-500 text-violet-500' : 'border-white'}`}>
+            <Link to="/" className={`flex py-2 w-3/6 justify-center items-center cursor-pointer transition-all rounded-l-md hover:bg-violet-500 hover:border-violet-500 hover:text-white border-b-2 ${route.pathname === '/' ? 'border-violet-500 text-violet-500' : 'border-neutral-100'}`}>
                 <ViewGridIcon className="w-7 mr-1"/>
                 Contacts
             </Link>
-            <Link to="/settings" className={`flex py-2 w-3/6 justify-center items-center cursor-pointer transition-all rounded-r-md hover:bg-violet-500 hover:border-violet-500 hover:text-white border-b-2 ${route.pathname === '/settings' ? 'border-violet-500 text-violet-500' : 'border-white'}`}>
+            <Link to="/settings" className={`flex py-2 w-3/6 justify-center items-center cursor-pointer transition-all rounded-r-md hover:bg-violet-500 hover:border-violet-500 hover:text-white border-b-2 ${route.pathname === '/settings' ? 'border-violet-500 text-violet-500' : 'border-neutral-100'}`}>
                 <CogIcon className="w-7 mr-1"/>
                 Settings
             </Link>
